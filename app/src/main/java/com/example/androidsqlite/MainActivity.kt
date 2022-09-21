77308dcefc7130597dc54e4cb7a989216722b452
garievdenis <gariev@webmx.ru>
2022-09-21 17:28:00 +0500

    add MyDbHelper, set random add contact in database

--- app/src/main/java/com/example/androidsqlite/MainActivity.kt
+++ app/src/main/java/com/example/androidsqlite/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.androidsqlite
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.BaseColumns
+import com.example.androidsqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: SQLiteDatabase
+
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        pr33
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        db = MyDbHelper(this).writableDatabase
+
+        binding.button.setOnClickListener {
+            val values = ContentValues().apply {
+                put(Contact.COLUMN_NAME, listOf("Ivan", "Egor", "Matvei").shuffled().first())
+                put(Contact.COLUMN_EMAIL, listOf("asdf@wegfe.ry", "fweg@greh.ry", "gtrhres@grjhtr.ry").shuffled().first())
+            }
+            val newContact = db.insert(Contact.TABLE_NAME, null, values)
+
+            val cursor = db.query(Contact.TABLE_NAME, null, "${BaseColumns._ID} = ?", arrayOf(newContact.toString()), null, null, null)
+
+            val cursor2 = db.rawQuery("select * from ${Contact.TABLE_NAME} where _id = ?", arrayOf(newContact.toString()))
+
+            cursor.moveToFirst()
+
+            val txt = "$newContact: ${cursor.getString(cursor.getColumnIndex(Contact.COLUMN_NAME))} ${cursor.getString(cursor.getColumnIndex(Contact.COLUMN_EMAIL))}"
+            cursor.close()
+            cursor2.close()
+            binding.tvMain.text = txt
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/androidsqlite/MyDbHelper.kt
+++ app/src/main/java/com/example/androidsqlite/MyDbHelper.kt
@@ -1,4 +1,38 @@
 package com.example.androidsqlite
 
-class MyDbHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+
+object Contact: BaseColumns {
+    const val TABLE_NAME = "contacts"
+    const val COLUMN_NAME = "name"
+    const val COLUMN_EMAIL = "email"
 }
+
+private const val SQL_CREATE = """
+    create table if not exists ${Contact.TABLE_NAME}
+    (${BaseColumns._ID} integer primary key autoincrement,
+    ${Contact.COLUMN_NAME} text,
+    ${Contact.COLUMN_EMAIL} text);"""
+
+private const val SQL_DELETE = "drop table ${Contact.TABLE_NAME};"
+
+class MyDbHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION ) {
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL(SQL_DELETE)
+        onCreate(db)
+    }
+
+    companion object {
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "my_database.db"
+    }
+
+}
+
